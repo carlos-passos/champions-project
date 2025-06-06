fc3b5c46c80b302133c4fdef4b89e2e84339d447
rafael <rafaelalves8596@gmail.com>
2025-06-06 10:17:59 -0300

    testes

---
+++ java/aula/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public class carro{
+
+    String nome;
+    String marca;
+    int ano;
+    int velocidade;
+    }
+    void acelerar(int aceleracao){
+        int vel -= aceleracao;
+    }
+    void frear(int reduzir){
+        int vel -= reduzir;
+    }
+    void buzinar() {
+        system
+    }
+}
